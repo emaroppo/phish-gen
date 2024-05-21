bf6fb8270df306943aa9aa0d9fa697fa8ed631c1
ubuntu <ema.roppo@gmail.com>
2024-05-21 19:59:46 +0200

    added methods for new pipeline (untested)

--- offline_finetuning/data/enron/EnronDataImporter.py
+++ offline_finetuning/data/enron/EnronDataImporter.py
@@ -8,7 +8,7 @@ class EnronDataImporter(DataImporter):
     def __init__(self, data_dir, db_name):
         super().__init__(data_dir, db_name)
 
-    def load_raw_data(self):
+    def step1_load_raw_data(self, collection="step1_raw_data"):
         for i in tqdm(os.listdir(self.data_dir)):
             if "inbox" in os.listdir(self.data_dir + "/" + i):
                 for j in tqdm(os.walk(self.data_dir + "/" + i + "/inbox")):
@@ -16,24 +16,42 @@ class EnronDataImporter(DataImporter):
                         with open(j[0] + "/" + k, "r", errors="ignore") as f:
                             text = f.read()
                             thread = EnronThread.from_text(
-                                text, j[0] + "/" + k, self.db_name, "raw_data"
+                                text, j[0] + "/" + k, self.db_name, collection
                             )
                             thread.save()
 
-    def isolate_multiparts(self, sample=None):
-        match_dict = {
-            "messages.body": {"$regex": "(-Original Message-)|(- Forwarded by)"}
-        }
-        if type(sample) == int:
-            multithread_docs = self.retrieve_samples(match_dict, n=sample)
-        elif sample is None:
-            multithread_docs = query_manager.connection[self.db_name]["raw_data"].find(
-                match_dict
-            )
-
-        query_manager.connection[self.db_name]["raw_data_multipart"].insert_many(
-            multithread_docs
+    def step2_split_messages(self):
+        
+        # copy collection from step 1
+        query_manager.connection[self.db_name]["step2_raw_data"].insert_many(
+            query_manager.connection[self.db_name]["step1_raw_data"].find()
         )
+
+        # isolate multipart, forwarded and single messages
+        match_dict = {
+            "messages.body": {"$regex": "(-Original Message-)"}
+        }
+        
+        self.split_collection("step2_raw_data", "step2_multipart", match_dict)
+
+        match_dict = {
+            "messages.body": {"$regex": "(- Forwarded by)"}
+        }
+        self.split_collection("step2_raw_data", "step2_forwarded", match_dict)
+
+        #rename the remaining collection to step2_single
+        query_manager.connection[self.db_name]["step2_single"].insert_many(
+            query_manager.connection[self.db_name]["step2_raw_data"].find()
+        )
+        query_manager.connection[self.db_name]["step2_raw_data"].drop()
+
+        #split multipart messages
+        for i in tqdm(
+            query_manager.connection[self.db_name]["step2_multipart"].find()
+        ):
+            thread = EnronThread.from_db(_id=i["_id"], db_name=self.db_name)
+            thread.split_messages()
+            thread.save()
 
     def clean_multiparts(self, sample=None):
 
@@ -52,3 +70,9 @@ class EnronDataImporter(DataImporter):
             )
             thread.clean()
             thread.save()
+
+    def pipeline(self):
+        self.load_raw_data()
+        self.isolate_multiparts()
+        self.clean_multiparts()
+        return
