ef13eecd0ae96f611060197e6ac5e3104acd2d0e
ubuntu <ema.roppo@gmail.com>
2024-05-21 19:58:32 +0200

    added pydantic model to DataImporter, got rid of obsolete methods, and added split_collection method for new pipeline

--- offline_finetuning/common_classes/DataImporter.py
+++ offline_finetuning/common_classes/DataImporter.py
@@ -1,32 +1,18 @@
 from common_classes.QueryManager import query_manager
+from pydantic import BaseModel
 import bson.json_util as json_util
 
 
-class DataImporter:
-    def __init__(self, data_dir, db_name):
-        self.data_dir = data_dir
-        self.db_name = db_name
+class DataImporter(BaseModel):
+    db_name: str
+    data_dir: str
 
     def save_raw(self, data):
         query_manager[self.db_name]["raw_data"].insert_one(data)
         return
 
-    def retrieve_samples(self, match_dict=dict(), collection="raw_data", n=1):
-        # aggregate pipeline to retrieve n samples from the collection
-        pipeline = [
-            {"$match": match_dict},
-            {"$sample": {"size": n}},
-        ]
-        return list(
-            query_manager.connection[self.db_name][collection].aggregate(pipeline)
-        )
-
-    def export_json(
-        self, collection="raw_data_multipart", query=dict(), path="data.json", n=500
-    ):
-        with open(path, "w") as f:
-            f.write("[\n")
-            for i in self.retrieve_samples(query, collection, n):
-                f.write(json_util.dumps(i) + ",\n")
-            f.write("]\n")
+    def split_collection(self, source_collection, target_collection, match_dict):
+        docs = query_manager[self.db_name][source_collection].find(match_dict)
+        query_manager[self.db_name][target_collection].insert_many(docs)
+        query_manager[self.db_name][source_collection].delete_many(match_dict)
         return
