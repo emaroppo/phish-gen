b72c9eaee88a7583976004a28bffe8904d6819b9
ubuntu <ema.roppo@gmail.com>
2024-07-23 14:42:57 +0200

    more utility functions

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from offline_finetuning.data_processing.enron.EnronDataImporter import EnronDataImporter
 from offline_finetuning.common_classes.DatasetFactory import DatasetFactory
-from transformers import AutoTokenizer
 from inference.MessageGenerator import MessageGenerator
+from offline_finetuning.auto_labelling.TopicMessageLabeller import TopicMessageLabeller
+from offline_finetuning.common_classes.QueryManager import query_manager
+from offline_finetuning.auto_labelling.NERMessageLabeller import NERMessageLabeller
 
 
 def run_pipeline(
-    db_name: str = "enron_emails_test4",
+    db_name: str = "enron_emails3",
     data_dir: str = "offline_finetuning/data_processing/enron/dataset/maildir",
 ):
 
@@ -27,19 +29,18 @@ def generate_doccamo_dataset(
 
 def generate_pytorch_dataset(
     databases: dict = {
-        "enron_emails_test4": ["step2_single", "step3_placeholders", "step2_multipart"]
+        "enron_emails3": ["step2_single", "step2_forwarded", "step2_multipart"]
     },
     from_files: bool = False,
 ):
     if from_files:
-        run_pipeline()
+        run_pipeline(
+            db_name="enron_emails3",
+            data_dir="offline_finetuning/data_processing/enron/dataset/maildir",
+        )
     dataset_factory = DatasetFactory(databases=databases)
-    tokenizer = AutoTokenizer.from_pretrained("gpt2")
-    tokenizer.pad_token = tokenizer.eos_token
 
     dataset = dataset_factory.generate_torch_dataset(
-        tokenizer=tokenizer,
-        max_length=512,
         save_path="offline_finetuning/datasets/pytorch/enron/",
     )
 
@@ -51,6 +52,7 @@ def generate_message(
     subject: str = "Budget Review Meeting",
     attachments: bool = False,
     urls: bool = True,
+    sentiment: list = ["neutral"],
     guided: str = False,
 ):
     message_generator = MessageGenerator(
@@ -58,9 +60,55 @@ def generate_message(
         quantized="4bit",
         adapter=f"offline_finetuning/models/gemma-2b/checkpoint-8000",
     )
-    message_generator.generate_message(
-        subject=subject, attachments=attachments, urls=urls, guided=guided
+    message = message_generator.generate_message(
+        subject=subject,
+        attachments=attachments,
+        urls=urls,
+        sentiment=sentiment,
+        guided=guided,
+    )
+
+    return message
+
+
+# retrieve all message bodies
+def retrieve_message_bodies(db_name: str, collections: list):
+    bodies = list()
+    for collection in collections:
+        bodies.extend(
+            [
+                thread["message"]["body"]
+                for thread in query_manager.connection[db_name][collection].aggregate(
+                    [{"$project": {"message": {"$arrayElemAt": ["$messages", -1]}}}]
+                )
+            ]
+        )
+    return bodies
+
+
+# retrieve a random message
+# print topic
+def retrieve_random_message(db_name: str, collections: list):
+    message = query_manager.connection[db_name][collections[0]].find_one()
+    topic_modelling = TopicMessageLabeller(
+        checkpoint_path="offline_finetuning/auto_labelling/topic_modelling/models/topic_model",
+    )
+    print(topic_modelling.label_message(message["messages"][-1]["body"]))
+
+
+# generate excels for entity validation
+
+
+def entity_validation(
+    classifier_id: str = "dslim/bert-base-NER",
+    collections: list = ["step2_single", "step2_multipart", "step2_forwarded"],
+    file_path: str = "offline_finetuning/auto_labelling/validation",
+):
+    ner_message_labeller = NERMessageLabeller(classifier_id=classifier_id, task="ner")
+    ner_message_labeller.generate_validation_excel(
+        collections=collections, file_path=file_path
     )
 
 
-generate_message()
+if __name__ == "__main__":
+    generate_pytorch_dataset(from_files=False)
