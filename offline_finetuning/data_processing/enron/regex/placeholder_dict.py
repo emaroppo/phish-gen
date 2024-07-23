ba4e91ff4636fa3baa8c6efa1f8c27c74dc6cd2d
ubuntu <ema.roppo@gmail.com>
2024-07-23 14:36:45 +0200

    added email addresses to manually extracted entities

--- offline_finetuning/data_processing/enron/regex/placeholder_dict.py
+++ offline_finetuning/data_processing/enron/regex/placeholder_dict.py
@@ -41,6 +41,14 @@ phone_numbers_dict = {
     ],
 }
 
+placeholder_emails_dict = {
+    "placeholder": "EMAIL",
+    "regex": [
+        r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+    ],
+}
+
+placeholder_dict["emails"] = placeholder_emails_dict
 placeholder_dict["urls"] = urls_dict
 placeholder_dict["attachments"] = attachments_dict
 placeholder_dict["dates"] = dates_dict
