3b6446c5ad98d052ce453406fc566a1a69304de2
ubuntu <ema.roppo@gmail.com>
2024-09-02 23:32:13 +0200

    refactored cleaning and processing pipelines. checkpointing before deleting obsolete code

---
+++ data/cleaning/CleaningPipeline.py
@@ -0,0 +1,240 @@
+from pydantic import BaseModel, Field
+from typing import List
+from offline_finetuning.common_classes.QueryManager import query_manager
+from offline_finetuning.common_classes.EmailMessage import EmailMessage
+from offline_finetuning.common_classes.EmailThread import EmailThread
+from bson import ObjectId
+from tqdm import tqdm
+import os
+import json
+import email
+import re
+
+with open(
+    "offline_finetuning/data_processing/enron/regex/thread_structure.json", "r"
+) as f:
+    thread_structure = json.load(f)
+
+with open("offline_finetuning/data_processing/enron/regex/noise.json", "r") as f:
+    noise = json.load(f)
+
+
+class CleaningPipeline(BaseModel):
+    database_name: str = "enron_datasource"
+
+    def load_raw_data(self, data_dir: str = "offline_finetuning/data_processing/enron/dataset/maildir"
+, out_collection: str = "raw_data"):
+        for i in tqdm(os.listdir(data_dir), desc="Loading raw inboxes"):
+            if "inbox" in os.listdir(data_dir + "/" + i):
+                for j in os.walk(data_dir + "/" + i + "/inbox"):
+                    for k in j[2]:
+                        with open(j[0] + "/" + k, "r", errors="ignore") as f:
+                            text = f.read()
+                            thread = EmailThread.from_text(
+                                text, j[0] + "/" + k, self.database_name, out_collection
+                            )
+                            thread.save()
+        return True
+
+    def extract_headers(self, thread_list: List[EmailThread]) -> List[EmailThread]:
+
+        for thread in tqdm(thread_list):
+            for message in thread.messages:
+                if message.headers is None:
+                    if message.is_main:
+                        message.headers = {
+                            k: v for k, v in email.message_from_string(message.body).items()
+                        }
+                        message.body = email.message_from_string(message.body).get_payload()
+                    else:
+                        pattern = thread_structure["headers"]["outer"]
+                        match = re.search(pattern, message.body)
+
+                        if match:
+                            from_header = match.group("From")
+                            subject_header = match.group("Subject")
+                            headers_block = match.group("Headers")
+
+                            intermediate_headers = dict(
+                                re.findall(
+                                    thread_structure["headers"]["inner"],
+                                    headers_block,
+                                    re.MULTILINE,
+                                )
+                            )
+
+                            headers = {"From": from_header, "Subject": subject_header}
+
+                            headers.update(intermediate_headers)
+
+                            email_content = re.sub(pattern, "", message.body, count=1)
+
+                            message.headers = headers
+                            message.body = email_content
+
+            thread.save()
+
+        return thread_list
+
+    def split_multipart_messages(
+        self,
+        thread_list: List[EmailThread],
+        message_split_regex: re.Pattern,
+    ) -> List[EmailThread]:
+
+        # Need to update this loop! using multiple separators would probably overwrite the messages
+        for thread in tqdm(thread_list):
+            message_thread = list()
+            for message in thread.messages:
+                split_message = message_split_regex.split(message.body)
+                if len(split_message) > 1:
+                    message.body = split_message.pop(0)
+                    previous_id = message.id
+                    message_thread.append(message)
+
+                    for i in split_message:
+                        message = EmailMessage.from_text(i, response=previous_id)
+                        previous_id = message.id
+                        message_thread.append(message)
+
+                
+                else:
+                    message_thread.append(message)
+
+            thread.messages = message_thread
+
+        return thread_list
+
+    def split_forwarded_messages(
+        self,
+        thread_list: List[EmailThread],
+        forwarded_regex: re.Pattern,
+    ):
+
+        for thread in tqdm(thread_list):
+            for message in thread.messages:
+                split_message = forwarded_regex.split(message.body)
+                # all threads seems to have at most one forwarded message, will update the code to handle multiple forwarded messages if needed
+                if len(split_message) > 1:
+                    message.body = split_message.pop(0)
+                    new_message = EmailMessage.from_text(
+                        split_message.pop(-1), forwarded_by=message.id
+                    )
+                    # find a way to preserve the order of the messages
+                    thread.messages.append(new_message)
+
+        return thread_list
+
+    def remove_noise(self, thread_list: List[EmailThread]):
+        for disclaimer_pattern in noise["disclaimer"]:
+            for thread in tqdm(thread_list):
+                for message in thread.messages:
+                    if re.search(disclaimer_pattern, message.body):
+                        message.disclaimer = re.search(
+                            disclaimer_pattern, message.body
+                        ).group()
+                        message.body = re.sub(disclaimer_pattern, "", message.body)
+
+        for footer_pattern in noise["footer"]:
+            for thread in tqdm(thread_list):
+                for message in thread.messages:
+                    if re.search(footer_pattern, message.body):
+                        message.body = re.sub(footer_pattern, "", message.body)
+
+        return thread_list
+
+    def clean_subject(self, thread_list: List[EmailThread]):
+        for thread in tqdm(thread_list):
+            for message in thread.messages:
+                if "Subject" in message.headers:
+                    message.headers["Subject"] = message.headers["Subject"].strip()
+                    message.headers["Subject"] = re.sub(
+                        r"\s+", " ", message.headers["Subject"]
+                    )
+                    message.headers["Subject"] = re.sub(
+                        r"\s*Fwd:\s*",
+                        "",
+                        message.headers["Subject"],
+                        flags=re.IGNORECASE,
+                    )
+                    message.headers["Subject"] = re.sub(
+                        r"\s*Re:\s*",
+                        "",
+                        message.headers["Subject"],
+                        flags=re.IGNORECASE,
+                    )
+
+        return thread_list
+
+    def run_pipeline(self):
+        self.load_raw_data()
+        # retrieve raw data
+        # extract headers
+        # save to a new collection (clean_data)
+
+        threads = query_manager.connection[self.database_name]["raw_data"].find()
+        raw_data = [EmailThread.deserialize(thread, db_name=self.database_name, collection="raw_data") for thread in threads]
+        cleaned_data = self.extract_headers(raw_data)
+        for i in cleaned_data:
+            i.save("single_messages")
+
+        # retrieve multipart messages
+        # copy them to a new collection
+        # delete them from the original collection
+        # split them into individual messages
+        # extract headers where is_main is False
+
+        for message_split_regex in thread_structure["reply_separator"]:
+            message_split_regex = re.compile(message_split_regex)
+            multipart_messages = query_manager.connection[self.database_name][
+                "single_messages"
+            ].find({"messages.body": {"$regex": message_split_regex}})
+            multipart_messages = [
+                EmailThread.deserialize(thread, db_name=self.database_name, collection="single_messages") for thread in multipart_messages
+            ]
+            multipart_messages = self.split_multipart_messages(
+                multipart_messages, message_split_regex
+            )
+            multipart_messages = self.extract_headers(multipart_messages)
+            for i in multipart_messages:
+
+                i.save(target_collection="multipart_messages")
+
+            multipart_messages_id = [ObjectId(i.id) for i in multipart_messages]
+            query_manager.connection[self.database_name]["single_messages"].delete_many(
+                {"_id": {"$in": multipart_messages_id}}
+            )
+
+        # retrieve forwarded messages
+        # delete them from the original collection
+        # split them into individual messages
+        # extract headers where is_main is False
+
+        for forwarded_regex in thread_structure["forwarded_separator"]:
+            forwarded_regex = re.compile(forwarded_regex)
+            for i in ["single_messages", "multipart_messages"]:
+                forwarded_messages = query_manager.connection[self.database_name][i].find(
+                    {"messages.body": {"$regex": forwarded_regex}})
+                forwarded_messages = [
+                    EmailThread.deserialize(thread, db_name=self.database_name, collection=i) for thread in forwarded_messages
+                ]
+                forwarded_messages = self.split_forwarded_messages(
+                    forwarded_messages, forwarded_regex
+                )
+                forwarded_messages = self.extract_headers(forwarded_messages)
+
+                for j in forwarded_messages:
+                    j.save("forwarded_messages")
+
+            forwarded_messages_id = [ObjectId(i.id) for i in forwarded_messages]
+            query_manager.connection[self.database_name]["single_messages"].delete_many(
+                {"_id": {"$in": forwarded_messages_id}}
+            )
+
+        # remove noise
+        for i in ["single_messages", "multipart_messages", "forwarded_messages"]:
+            threads = query_manager.connection[self.database_name][i].find()
+            threads = [EmailThread.deserialize(thread, db_name=self.database_name, collection=i) for thread in threads]
+            cleaned_data = self.remove_noise(threads)
+            for j in cleaned_data:
+                j.save(target_collection=i)
---
+++ data/processing/ProcessingPipeline.py
@@ -0,0 +1,326 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from offline_finetuning.common_classes.EmailThread import EmailThread
+from offline_finetuning.common_classes.EmailMessage import EmailMessage
+import re
+import json
+from tqdm import tqdm
+from data.processing.labellers.NERMessageLabeller import NERMessageLabeller
+from data.processing.labellers.SentimentMessageLabeller import (
+    SentimentMessageLabeller,
+)
+from data.processing.labellers.TopicMessageLabeller import TopicMessageLabeller
+from offline_finetuning.common_classes.QueryManager import query_manager
+
+
+with open("offline_finetuning/data_processing/enron/regex/placeholders.json", "r") as f:
+    placeholders = json.load(f)
+
+
+def message_data(messages: List[EmailMessage]):
+    for message in messages:
+        yield message.body
+
+
+class ProcessingPipeline(BaseModel):
+    db: str = "enron_datasource"
+
+    def check_html(self, thread_list: List[EmailThread]) -> List[EmailThread]:
+        for thread in thread_list:
+            for message in thread.messages:
+                if re.search(r"<html>", message.body):
+                    message.is_html = True
+        return thread_list
+
+    def get_word_count(self, thread_list: List[EmailThread]) -> List[EmailThread]:
+        for thread in thread_list:
+            for message in thread.messages:
+                message.word_count = len(message.body.split())
+        return thread_list
+
+    def get_attachments_formats(
+        self, thread_list: List[EmailThread]
+    ) -> List[EmailThread]:
+        for thread in thread_list:
+            for message in thread.messages:
+                if message.entities is not None and "manual" in message.entities:
+                    if "ATTACHMENT" in message.entities["manual"]:
+                        attachments = [
+                            entity[0]
+                            for entity in message.entities["manual"]["ATTACHMENT"]
+                        ]
+
+                        # remove >> and/or ) from the attachment name
+                        message.attachments_format = [
+                            attachment.split(".")[-1] for attachment in attachments
+                        ]
+                        message.attachments_format = [
+                            attachment.split(")")[0].lower()
+                            for attachment in message.attachments_format
+                        ]
+                        message.attachments_format = [
+                            attachment.split(">>")[0].lower()
+                            for attachment in message.attachments_format
+                        ]
+        return thread_list
+
+    def extract_entities(
+        self, thread_list: List[EmailThread], placeholder: str, regex: str
+    ):
+        for thread in thread_list:
+            for message in thread.messages:
+                matches = re.finditer(regex, message.body)
+                extracted_entities = list()
+
+                for match in matches:
+                    value = match.group()
+                    label = placeholder
+                    start = match.start()
+                    end = match.end()
+                    message.add_entity(
+                        start=start,
+                        end=end,
+                        entity_value=value,
+                        entity_type=label,
+                        detection_method="manual",
+                    )
+
+        return thread_list
+
+    def remove_overlapping_labels(self, thread_list: List[EmailThread]):
+        for thread in thread_list:
+            for message in thread.messages:
+                labels = list()
+
+                if message.entities is not None:
+                    for detection_method in message.entities:
+                        if message.entities[detection_method] is not None:
+                            for entity_type in message.entities[detection_method]:
+                                labels.extend(
+                                    [
+                                        (detection_method, entity_type, label)
+                                        for label in message.entities[detection_method][
+                                            entity_type
+                                        ]
+                                    ]
+                                )
+
+                labels.sort(key=lambda x: (x[2][1], -len(x[2][0])))
+
+                # Initialize a list to hold non-overlapping labels
+                non_overlapping_labels = []
+
+                # Initialize the end index of the last added label to -1
+                last_end_idx = -1
+
+                for label in labels:
+                    _, start_idx, end_idx = label[2]
+
+                    # If the current label does not overlap with the last added label, add it to the list
+                    if start_idx > last_end_idx:
+                        non_overlapping_labels.append(label)
+                        last_end_idx = end_idx
+                # reconstruct the entities dictionary
+                message.entities = dict()
+                for label in non_overlapping_labels:
+
+                    detection_method, entity_type, entity = label
+                    if detection_method not in message.entities:
+                        message.entities[detection_method] = dict()
+                    if entity_type not in message.entities[detection_method]:
+                        message.entities[detection_method][entity_type] = list()
+
+                    message.entities[detection_method][entity_type].append(entity)
+
+        return thread_list
+
+    def predict_sentiment(
+        self,
+        message_list: List[EmailMessage],
+        sentiment_predictor: SentimentMessageLabeller,
+        batch_size: int = 128,
+    ):
+
+        sentiment = sentiment_predictor.label_message(
+            message_data(message_list), batch_size=batch_size
+        )
+        for message, sentiment in zip(message_list, sentiment):
+            message.add_sentiment(sentiment)
+        return message_list
+
+    def predict_topic(
+        self,
+        message_list: List[EmailMessage],
+        topic_predictor: TopicMessageLabeller,
+        save=True,
+    ):
+        topic = topic_predictor.label_message(message_data(message_list))
+        for message, topic in zip(message_list, topic):
+            message.add_topic(topic)
+            message.save()
+
+    def extract_named_entities(
+        self,
+        message_list: List[EmailMessage],
+        entity_predictor: NERMessageLabeller,
+        batch_size: int = 128,
+    ) -> List[EmailMessage]:
+
+        message_bodies = [message.body for message in message_list]
+        extracted_entities = entity_predictor.label_message(
+            message_bodies, batch_size=batch_size
+        )
+        for message, extracted_entities in zip(message_list, extracted_entities):
+            if extracted_entities:
+                for entity in extracted_entities:
+                    start, end, label = entity
+                    entity_value = message.body[start:end]
+                    message.add_entity(
+                        entity_type=label,
+                        start=start,
+                        end=end,
+                        entity_value=entity_value,
+                        detection_method="auto",
+                    )
+
+        return message_list
+
+    def insert_entity_placeholders(
+        message_list: List[EmailMessage],
+        entity_types: List[str] = [
+            "URL",
+            "ATTACHMENT",
+            "PHONE",
+            "DATE",
+            "PER",
+            "ORG",
+            "EMAIL",
+        ],
+        manual=True,
+        auto=False,
+    ):
+
+        for message in message_list:
+            entity_labels = list()
+            for entity_type in entity_types:
+                if manual and entity_type in message.entities["manual"]:
+                    entity_labels.extend(
+                        [
+                            (label, entity_type)
+                            for label in message.entities["manual"][entity_type]
+                        ]
+                    )
+                if auto and entity_type in message.entities["auto"]:
+                    entity_labels.extend(
+                        [
+                            (label, entity_type)
+                            for label in message.entities["auto"][entity_type]
+                        ]
+                    )
+
+            entity_labels.sort(key=lambda x: x[0][2], reverse=True)
+            body = message.body
+            for label in entity_labels:
+                body = body[: label[0][1]] + f"<{label[1]}>" + body[label[0][2] :]
+            message.body = body
+        return message_list
+
+    def run_features_pipeline(self, collections: List[str]):
+
+        for collection in collections:
+            # find all threads
+            """data = query_manager.connection[self.db][collection].find()
+            thread_list = [
+                EmailThread.deserialize(thread, db_name=self.db, collection=collection)
+                for thread in data
+            ]
+            thread_list = self.check_html(thread_list)
+            thread_list = self.get_word_count(thread_list)
+
+            for thread in thread_list:
+                thread.save()
+
+            # retrieve all message bodies and ids
+            pipeline = [
+                {"$project": {"messages": 1}},
+                {"$unwind": "$messages"},
+            ]
+
+            message_list = query_manager.connection[self.db][collection].aggregate(
+                pipeline
+            )
+            message_list = [
+                EmailMessage.deserialize(message["messages"])
+                for message in message_list
+            ]
+
+            sentiment_labeller = SentimentMessageLabeller(
+                classifier_id="michellejieli/emotion_text_classifier",
+                task="sentiment-analysis",
+                no_top_k=True,
+            )
+
+            ner_labeller = NERMessageLabeller(
+                classifier_id="dslim/bert-large-NER",
+                task="ner",
+            )
+
+            # sentiment analysis
+            message_list = self.predict_sentiment(
+                message_list=message_list, sentiment_predictor=sentiment_labeller
+            )
+
+            # extract entities
+            message_list = self.extract_named_entities(
+                message_list=message_list, entity_predictor=ner_labeller
+            )
+
+            for message in tqdm(message_list):
+                message.save(db_name=self.db, target_collection=collection)"""
+
+            for k, v in tqdm(placeholders.items()):
+                for regex in v["regex"]:
+                    thread_list = query_manager.connection[self.db][collection].find(
+                        {"messages.body": {"$regex": regex}}
+                    )
+
+                    thread_list = [
+                        EmailThread.deserialize(
+                            thread, db_name=self.db, collection=collection
+                        )
+                        for thread in thread_list
+                    ]
+
+                    thread_list = self.extract_entities(
+                        thread_list,
+                        v["placeholder"],
+                        regex,
+                    )
+
+                    for thread in thread_list:
+                        thread.save()
+
+            # retrieve messages with attachments
+            thread_list = query_manager.connection[self.db][collection].find(
+                {"messages.entities.manual.ATTACHMENT": {"$exists": True}}
+            )
+            thread_list = [
+                EmailThread.deserialize(thread, db_name=self.db, collection=collection)
+                for thread in thread_list
+            ]
+            thread_list = self.get_attachments_formats(thread_list)
+            for thread in thread_list:
+                thread.save()
+
+            # remove overlapping labels
+            # retrieve all threads
+            data = query_manager.connection[self.db][collection].find()
+            thread_list = [
+                EmailThread.deserialize(thread, db_name=self.db, collection=collection)
+                for thread in data
+            ]
+            thread_list = self.remove_overlapping_labels(thread_list)
+            for thread in tqdm(thread_list):
+                thread.save()
+
+        return True
