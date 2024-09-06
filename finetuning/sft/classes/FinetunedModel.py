918dc352c3d611558c51a1c88ead28a7a8725c16
ubuntu <ema.roppo@gmail.com>
2024-09-06 14:24:15 +0200

    spotted major mistake in embedding initialization (fixed hopefully), added option to set warmup steps via argument, fixed minor error in how model checkpoints were saved in the database

--- finetuning/sft/classes/FinetunedModel.py
+++ finetuning/sft/classes/FinetunedModel.py
@@ -40,7 +40,7 @@ class CheckpointCallback(TrainerCallback):
         self.finetuned_model = finetuned_model
 
     def on_save(self, args, state, control, **kwargs):
-        timestamp = int(datetime.now().timestamp())
+        timestamp = self.finetuned_model.timestamp
         steps = state.global_step
         self.finetuned_model.add_checkpoint(
             timestamp, self.finetuned_model.base_model_id, steps
@@ -60,6 +60,7 @@ class FinetunedModel(BaseModel):
     rank: int
     quantization: str
     batch_size: int
+    warmup_steps: int = 0
     gradient_accumulation_steps: int
     learning_rate: float
     checkpoints: Optional[List[FinetunedCheckpoint]] = list()
@@ -109,8 +110,22 @@ class FinetunedModel(BaseModel):
         return cls.deserialize(finetuned_model)
 
     @classmethod
+    def get_sentence_embeddings(cls, model, tokenizer, sentence: str):
+        inputs = tokenizer(
+            sentence,
+            return_tensors="pt",
+            padding="max_length",
+            max_length=512,
+            truncation=True,
+        )
+        with torch.no_grad():
+            outputs = model(**inputs, output_hidden_states=True)
+            last_hidden_state = outputs.hidden_states[-1]
+        return last_hidden_state.mean(dim=1)
+
+    @classmethod
     def initialize_new_embeddings(
-        model, tokenizer, related_tokens_dict, custom_tokens=[]
+        cls, model, tokenizer, related_tokens_dict, custom_tokens=[], unk_token_id=3
     ):
         # Get the embedding layer
         embedding_layer = model.get_input_embeddings()
@@ -120,9 +135,32 @@ class FinetunedModel(BaseModel):
             related_tokens = related_tokens_dict.get(token, [])
             if related_tokens:
                 related_token_ids = [
-                    tokenizer.convert_tokens_to_ids(rt) for rt in related_tokens
+                    tokenizer(rt, add_special_tokens=False) for rt in related_tokens
                 ]
-                related_embeddings = embedding_layer.weight.data[related_token_ids]
+                single_tokens = [
+                    related_token_id
+                    for related_token_id in related_token_ids
+                    if len(related_token_id) == 1
+                ]
+                compound_tokens = [
+                    related_tokens[i]
+                    for i in range(len(related_token_ids))
+                    if len(related_token_ids[i]) > 1
+                ]
+                if single_tokens:
+                    single_tokens = embedding_layer.weight.data[single_tokens]
+
+                if compound_tokens:
+                    compound_tokens = [
+                        cls.get_sentence_embeddings(model, tokenizer, compound_token)
+                        for compound_token in compound_tokens
+                    ]
+                if single_tokens and compound_tokens:
+                    related_embeddings = torch.cat([single_tokens, compound_tokens])
+                elif single_tokens:
+                    related_embeddings = single_tokens
+                elif compound_tokens:
+                    related_embeddings = torch.stack(compound_tokens)
                 average_embedding = related_embeddings.mean(dim=0)
             else:
                 # If no related tokens, initialize with random embeddings
@@ -131,6 +169,8 @@ class FinetunedModel(BaseModel):
             # Add the new token to the tokenizer and set its embedding
             tokenizer.add_tokens([token])
             token_id = tokenizer.convert_tokens_to_ids(token)
+            model.resize_token_embeddings(len(tokenizer))
+            embedding_layer = model.get_input_embeddings()
             embedding_layer.weight.data[token_id] = average_embedding
 
     @classmethod
@@ -142,11 +182,13 @@ class FinetunedModel(BaseModel):
         rank: int = 16,
         quantization: Union[Literal["4bit", "8bit"], None] = "4bit",
         batch_size: int = 4,
+        warmup_steps: int = 0,
         gradient_accumulation_steps: int = 4,
         learning_rate: float = 2e-4,
         custom_tokens: Union[List[str], Dict[str, List[str]], None] = None,
         save_steps: int = 500,
     ):
+        custom_tokens_dict = None
         if type(custom_tokens) == dict:
             custom_tokens_dict = custom_tokens
             custom_tokens = custom_tokens_dict.keys()
@@ -161,6 +203,7 @@ class FinetunedModel(BaseModel):
             batch_size=batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
             learning_rate=learning_rate,
+            warmup_steps=warmup_steps,
             custom_tokens=custom_tokens,
         )
         finetuned_model.save()
@@ -181,17 +224,16 @@ class FinetunedModel(BaseModel):
         tokenizer = AutoTokenizer.from_pretrained(base_model_id)
         tokenizer.pad_token = tokenizer.eos_token
 
-        if custom_tokens:
-            if type(custom_tokens) == list:
-                tokenizer.add_tokens(custom_tokens)
-                model.resize_token_embeddings(len(tokenizer))
-            elif type(custom_tokens) == dict:
-                FinetunedModel.initialize_new_embeddings(
-                    model,
-                    tokenizer,
-                    custom_tokens=custom_tokens,
-                    related_tokens_dict=custom_tokens_dict,
-                )
+        if custom_tokens_dict:
+            FinetunedModel.initialize_new_embeddings(
+                model,
+                tokenizer,
+                custom_tokens=custom_tokens,
+                related_tokens_dict=custom_tokens_dict,
+            )
+        elif custom_tokens:
+            tokenizer.add_tokens(custom_tokens)
+            model.resize_token_embeddings(len(tokenizer))
 
         # Load dataset
         dataset = dataset.load_finetuning_dataset(tokenizer=tokenizer)
@@ -219,6 +261,7 @@ class FinetunedModel(BaseModel):
         args = TrainingArguments(
             per_device_train_batch_size=batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
+            warmup_steps=warmup_steps,
             num_train_epochs=epochs,
             save_steps=save_steps,
             learning_rate=learning_rate,
