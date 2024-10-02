c718025f7e6855b7c78d3d5c1262cdc13f06c6c6
ubuntu <ema.roppo@gmail.com>
2024-10-02 12:46:05 +0200

    added some useful functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import List, Dict
 from finetuning.sft.classes.Experiment import Experiment
 from finetuning.sft.classes.FinetuningDataset import FinetuningDataset
+from data.DatasetExporter import DatasetExporter
+import json
 
 
 def create_dataset(file_list: List[str]):
@@ -8,7 +10,15 @@ def create_dataset(file_list: List[str]):
     return dataset
 
 
-def queue_experiments(arg_list: List[Dict], dataset_timestamp: int = 1722274538):
+def create_ner_validation_dataset():
+    exporter = DatasetExporter()
+    exporter.export_validation_dataset(
+        headers=False, manual=True, auto=True, timestamp=1726055316
+    )
+    return True
+
+
+def queue_experiments(arg_list: List[Dict], dataset_timestamp: int = 1727172426):
     experiments = list()
     for arg_dict in arg_list:
         experiment = Experiment.run_experiment(
@@ -20,8 +30,22 @@ def queue_experiments(arg_list: List[Dict], dataset_timestamp: int = 1722274538)
     return experiments
 
 
-if __name__ == "__main__":
+def resume_experiment(
+    finetune_timestamp: int,
+    dataset_timestamp: int,
+    epochs: int,
+    model_id: str = "google/gemma-2b",
+):
+    experiment = Experiment.from_db(
+        model_id=model_id,
+        model_timestamp=finetune_timestamp,
+        dataset_timestamp=dataset_timestamp,
+    )
+    experiment.finetuned_model.resume_training(epochs=epochs)
+    return experiment
 
+
+if __name__ == "__main__":
     exp1 = {
         "custom_tokens": [
             "<URL>",
