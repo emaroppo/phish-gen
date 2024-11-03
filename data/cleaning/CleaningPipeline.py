ee5d69228a6437e661f19a635a55d23a92cf7b45
ubuntu <ema.roppo@gmail.com>
2024-11-03 15:57:35 +0100

    code formatting

--- data/cleaning/CleaningPipeline.py
+++ data/cleaning/CleaningPipeline.py
@@ -19,7 +19,9 @@ with open("data/regex/noise.json", "r") as f:
 class CleaningPipeline(BaseModel):
     database_name: str = "enron_datasource"
 
-    def load_raw_data(self, path_list: List[str], out_collection: str = "single_messages"):
+    def load_raw_data(
+        self, path_list: List[str], out_collection: str = "single_messages"
+    ):
         for path in tqdm(path_list, desc="Loading files"):
             try:
                 with open(path, "r", errors="ignore") as f:
@@ -27,13 +29,14 @@ class CleaningPipeline(BaseModel):
                         email_message = email.message_from_file(f)
                     else:
                         email_message = email.message_from_string(f.read())
-                
+
                 if email_message.is_multipart():
                     for part in email_message.walk():
                         if part.get_content_type() == "text/plain":
                             try:
                                 thread_body = part.get_payload(decode=True).decode(
-                                    part.get_content_charset(failobj="utf-8"), errors="ignore"
+                                    part.get_content_charset(failobj="utf-8"),
+                                    errors="ignore",
                                 )
                             except LookupError:
                                 thread_body = part.get_payload(decode=True).decode(
@@ -42,19 +45,18 @@ class CleaningPipeline(BaseModel):
                             break
                 else:
                     thread_body = email_message.get_payload(decode=True).decode(
-                        email_message.get_content_charset(failobj="utf-8"), errors="ignore"
+                        email_message.get_content_charset(failobj="utf-8"),
+                        errors="ignore",
                     )
-                
+
                 thread = EmailThread.from_text(
                     thread_body, path, self.database_name, out_collection
                 )
                 thread.messages[0].is_main = True
-                thread.messages[0].headers = {
-                    k: v for k, v in email_message.items()
-                }
+                thread.messages[0].headers = {k: v for k, v in email_message.items()}
                 if not thread.save():
                     print(f"Failed to save thread from {path}")
-            
+
             except UnicodeDecodeError:
                 raise
                 continue
@@ -64,7 +66,7 @@ class CleaningPipeline(BaseModel):
             except Exception as e:
                 print(f"An error occurred while processing {path}: {e}")
                 continue
-        
+
         return True
 
     def parse_alt_separator(self, separator_string: str) -> dict:
@@ -74,46 +76,58 @@ class CleaningPipeline(BaseModel):
             return {"Sent": match.group("date"), "From": match.group("sender")}
         else:
             return None
-    
+
     def parse_headers_string(self, header_string: str) -> dict:
-        header_parsing_regular_expression= r"^\*?([\w\-]*?):\*?"
-        header_parsing_regular_expression = re.compile(header_parsing_regular_expression, re.MULTILINE)
+        header_parsing_regular_expression = r"^\*?([\w\-]*?):\*?"
+        header_parsing_regular_expression = re.compile(
+            header_parsing_regular_expression, re.MULTILINE
+        )
         headers = header_parsing_regular_expression.split(header_string, maxsplit=0)
         header_fields = headers[1::2]
         header_values = headers[2::2]
-        headers = {k:v for k,v in zip(header_fields, header_values)}
+        headers = {k: v for k, v in zip(header_fields, header_values)}
         return headers
 
     def extract_headers_single(self, message_body: str) -> dict:
         header_splitting_regular_expression = r"(?:_+)?\s*(\*?From:\*?.*?[\n\r](?:.*?[\n\r])*?\*?Subj(?:ect)?:\*?([\w\s.,!?;:'\"@\—\-\–\[\]\(\)/$’+&%#|“”]+?)?)[\n\r]{2,}"
-        header_splitting_regular_expression = re.compile(header_splitting_regular_expression, re.MULTILINE|re.DOTALL)        
+        header_splitting_regular_expression = re.compile(
+            header_splitting_regular_expression, re.MULTILINE | re.DOTALL
+        )
         header_string = header_splitting_regular_expression.search(message_body)
-        
+
         try:
             header_string = header_string.group(1)
         except AttributeError:
             print(message_body)
 
-        #remove the header string from the thread string
-        message_body = header_splitting_regular_expression.sub("", message_body, count=1)
+        # remove the header string from the thread string
+        message_body = header_splitting_regular_expression.sub(
+            "", message_body, count=1
+        )
 
         headers = self.parse_headers_string(header_string)
         return headers, message_body
-    
+
     def extract_headers(self, thread_list: List[EmailThread]) -> List[EmailThread]:
         header_splitting_regular_expression = r"(?:_+)?\s*(\*?From:\*?.*?[\n\r](?:.*?[\n\r])*?\*?Subj(?:ect)?:\*?([\w\s.,!?;:'\"@\—\-\–\[\]\(\)/$’+&%#|“”]+?)?)[\n\r]{2,}"
 
-        header_splitting_regular_expression = re.compile(header_splitting_regular_expression)
+        header_splitting_regular_expression = re.compile(
+            header_splitting_regular_expression
+        )
         for thread in tqdm(thread_list, desc="Extracting headers"):
             print(thread.id)
             for message in thread.messages:
-                if message.headers is None and re.match(header_splitting_regular_expression, message.body):
-                    message.headers, message.body = self.extract_headers_single(message.body+"\n\n")
+                if message.headers is None and re.match(
+                    header_splitting_regular_expression, message.body
+                ):
+                    message.headers, message.body = self.extract_headers_single(
+                        message.body + "\n\n"
+                    )
 
             thread.save()
 
         return thread_list
-  
+
     def split_multipart_messages(
         self,
         thread_list: List[EmailThread],
@@ -142,7 +156,7 @@ class CleaningPipeline(BaseModel):
         thread_list = self.clean_bodies(thread_list)
         thread_list = self.extract_headers(thread_list)
         return thread_list
-    
+
     def split_message_alt_single(self, message_body: str, thread_subject: str):
         alt_separator = r"(On\s+(?:(?:(?:Mon(?:day)?|Tues(?:day)?|Wed(?:nesday)?|Thu(?:rsday)?|Fri(?:day)?|Sat(?:urday)?|Sun(?:day)?),?\s+)?(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|June?|July?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+\d+,\s+\d{4}(?:,?\s+(?:at\s+)?\d+:\d+\s+(?:AM|PM))?),?\s+(?:.+?(?:\s*<\s*.+)?)\s+wrote:)"
         alt_separator = re.compile(alt_separator)
@@ -159,11 +173,14 @@ class CleaningPipeline(BaseModel):
             headers = self.parse_alt_separator(header_string)
             headers["Subject"] = thread_subject
             header_dicts.append(headers)
-        
-        thread_messages = [{"header": header, "body": body} for header, body in zip(header_dicts, bodies_list)]
-        
+
+        thread_messages = [
+            {"header": header, "body": body}
+            for header, body in zip(header_dicts, bodies_list)
+        ]
+
         return first_message, thread_messages
-    
+
     def split_message_alt(self, thread_list: List[EmailThread]):
         thread_list = self.clean_bodies(thread_list)
         for thread in tqdm(thread_list, desc="Splitting messages on headers"):
@@ -177,7 +194,9 @@ class CleaningPipeline(BaseModel):
                     subject = message.headers["Subject"]
                 else:
                     subject = main_subject
-                new_message_body, new_messages = self.split_message_alt_single(message.body, subject)
+                new_message_body, new_messages = self.split_message_alt_single(
+                    message.body, subject
+                )
 
                 if len(new_messages) > 0:
                     message.body = new_message_body
@@ -196,13 +215,16 @@ class CleaningPipeline(BaseModel):
                     message_thread.append(message)
         thread_list = self.clean_bodies(thread_list)
         return thread_list
-    
+
     def split_on_headers_single(self, thread_string):
         header_splitting_regular_expression = r"(?:_+)?\s+(\*?From:\*?\s(?:.*)\n(?:[\s\S]*?)\n\*?Subj(?:ect)?:\*?\s(?:.*))"
-        header_splitting_regular_expression = re.compile(header_splitting_regular_expression)
+        header_splitting_regular_expression = re.compile(
+            header_splitting_regular_expression
+        )
 
-
-        thread_messages = header_splitting_regular_expression.split(thread_string, maxsplit=0)
+        thread_messages = header_splitting_regular_expression.split(
+            thread_string, maxsplit=0
+        )
         first_message = thread_messages.pop(0)
         header_list = thread_messages[::2]
         bodies_list = thread_messages[1::2]
@@ -212,7 +234,10 @@ class CleaningPipeline(BaseModel):
             headers = self.parse_headers_string(header_string)
             header_dicts.append(headers)
 
-        thread_messages = [{"header": header, "body": body} for header, body in zip(header_dicts, bodies_list)]
+        thread_messages = [
+            {"header": header, "body": body}
+            for header, body in zip(header_dicts, bodies_list)
+        ]
         thread_messages.insert(0, {"header": None, "body": first_message})
         return first_message, thread_messages
 
@@ -221,14 +246,16 @@ class CleaningPipeline(BaseModel):
         for thread in tqdm(thread_list, desc="Splitting messages on headers"):
             message_thread = list()
             for message in thread.messages:
-                
-                new_message_body, new_messages = self.split_on_headers_single(message.body)
-                
+
+                new_message_body, new_messages = self.split_on_headers_single(
+                    message.body
+                )
+
                 if len(new_messages) > 0:
                     message.body = new_message_body
                     previous_id = message.id
                     message_thread.append(message)
-                    #pair body with headers strings from headers capture group
+                    # pair body with headers strings from headers capture group
                     for i in new_messages:
                         headers = i["header"]
                         body = i["body"]
@@ -258,7 +285,6 @@ class CleaningPipeline(BaseModel):
                     # find a way to preserve the order of the messages
                     thread.messages.append(new_message)
 
-        
         self.clean_bodies(thread_list)
         self.extract_headers(thread_list)
 
@@ -326,9 +352,9 @@ class CleaningPipeline(BaseModel):
         return thread_list
 
     def run_pipeline(self, path_list: List[str] = None):
-        """if path_list:
-            self.load_raw_data(path_list)"""
-    
+        if path_list:
+            self.load_raw_data(path_list)
+
         threads = query_manager.connection[self.database_name]["single_messages"].find()
         threads = [
             EmailThread.deserialize(
@@ -336,7 +362,7 @@ class CleaningPipeline(BaseModel):
             )
             for thread in threads
         ]
-        
+
         # retrieve multipart messages
         # copy them to a new collection
         # delete them from the original collection
@@ -357,9 +383,9 @@ class CleaningPipeline(BaseModel):
             multipart_messages = self.split_multipart_messages(
                 multipart_messages, message_split_regex
             )
-            
+
             multipart_messages = self.extract_headers(multipart_messages)
-            
+
             for i in tqdm(multipart_messages):
 
                 i.save(target_collection="multipart_messages")
@@ -368,8 +394,6 @@ class CleaningPipeline(BaseModel):
             query_manager.connection[self.database_name]["single_messages"].delete_many(
                 {"_id": {"$in": multipart_messages_id}}
             )
-
-        
 
         # retrieve forwarded messages
         # delete them from the original collection
@@ -400,14 +424,16 @@ class CleaningPipeline(BaseModel):
                     {"_id": {"$in": forwarded_messages_id}}
                 )
 
-        #split on headers
+        # split on headers
 
         for i in ["single_messages", "multipart_messages", "forwarded_messages"]:
-            
-            #TODO: add filter to only retrieve messages that match the pattern
+
+            # TODO: add filter to only retrieve messages that match the pattern
             header_splitting_regular_expression = r"(?:_+)?\s*(\*?From:\*?.*?[\n\r](?:.*?[\n\r])*?\*?Subj(?:ect)?:\*?([\w\s.,!?;:'\"@\—\-\–\[\]\(\)/$’+*&%#\|“”\‘\’]+?)?)[\n\r]{2,}"
 
-            threads = query_manager.connection[self.database_name][i].find({"messages.body": {"$regex": header_splitting_regular_expression}})
+            threads = query_manager.connection[self.database_name][i].find(
+                {"messages.body": {"$regex": header_splitting_regular_expression}}
+            )
             threads = [
                 EmailThread.deserialize(
                     thread, db_name=self.database_name, collection=i
@@ -424,10 +450,12 @@ class CleaningPipeline(BaseModel):
                 {"_id": {"$in": multipart_messages_id}}
             )
 
-        #split on alt pattern:
+        # split on alt pattern:
 
         alt_separator = r"(On\s+(?:(?:(?:Mon(?:day)?|Tue(?:sday)?|Wed(?:nesday)?|Thu(?:rsday)?|Fri(?:day)?|Sat(?:urday)?|Sun(?:day)?),?\s+)?(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|June?|July?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+\d+,?\s+\d{4}(?:,?\s+(?:at\s+)?\d+:\d+\s+(?:AM|PM))?),?\s+(?:.+?(?:\s*<\s+.+)?)\s+wrote:)"
-        thread_list=query_manager.connection[self.database_name]["single_messages"].find({"messages.body": {"$regex": alt_separator}})
+        thread_list = query_manager.connection[self.database_name][
+            "single_messages"
+        ].find({"messages.body": {"$regex": alt_separator}})
         thread_list = [
             EmailThread.deserialize(
                 thread, db_name=self.database_name, collection="single_messages"
@@ -443,7 +471,6 @@ class CleaningPipeline(BaseModel):
         query_manager.connection[self.database_name]["single_messages"].delete_many(
             {"_id": {"$in": multipart_messages_id}}
         )
-        
 
         # remove noise
         for i in ["single_messages", "multipart_messages", "forwarded_messages"]:
