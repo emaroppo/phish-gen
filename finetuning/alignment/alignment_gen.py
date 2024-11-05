02848e1386245ef401e3c08c85d29cad85fb620e
ubuntu <ema.roppo@gmail.com>
2024-11-05 16:41:54 +0100

    fixes and efficiency improvements

--- finetuning/alignment/alignment_gen.py
+++ finetuning/alignment/alignment_gen.py
@@ -1,18 +1,19 @@
 import streamlit as st
-
 from inference.MessageGenerator import MessageGenerator
-from finetuning.sft.classes.FinetunedModel import FinetunedModel
+from finetuning.sft.classes.finetuned_models.FinetunedLLama31 import FinetunedLLama31
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["dpo_dataset"]
 
 
-finetuned_model = FinetunedModel.from_db(
-    base_model_id="google/gemma-2b", timestamp=1727175863
-)
+@st.cache_resource
+def load_message_generator():
+    finetuned_model = FinetunedLLama31.from_db(timestamp=1730656372)
+    return MessageGenerator(finetuned_model=finetuned_model, checkpoint=2000)
 
-message_generator = MessageGenerator(finetuned_model=finetuned_model, checkpoint=1500)
+
+message_generator = load_message_generator()
 
 st.title("DPO Generator")
 st.write(
@@ -34,11 +35,14 @@ prompt = {
     "attachments": attachments,
     "sentiment": sentiment,
 }
-# press button to generate a messge pair
+# press button to generate a message pair
 button = st.button("Generate Message")
 if button:
     message = message_generator.generate_message(**prompt)
+    st.session_state.generated_message = message
 
+if "generated_message" in st.session_state:
+    message = st.session_state.generated_message
     st.write("Generated Message:")
 
     col1, col2 = st.columns(2)
@@ -49,11 +53,21 @@ if button:
     with col2:
         st.write("Revised Message")
         edited_message = st.text_area("Edit Message", value=message)
-        button_2 = st.button("Select Revision")
-        if button_2:
-            db["dpo_dataset"].insert_one(
-                {"chosen": edited_message, "rejected": message}
-            )
+        if st.button("Select Revision", key="select_revision"):
+            st.session_state.selected_revision = edited_message
+            st.session_state.original_message = message
 
-    # press neither button to generate a new message pair
-    st.button("Neither")
+if "selected_revision" in st.session_state:
+    db["dpo_dataset"].insert_one(
+        {
+            "chosen": st.session_state.selected_revision,
+            "rejected": st.session_state.original_message,
+        }
+    )
+    st.write("Inserted into database")
+    del st.session_state.selected_revision
+    del st.session_state.original_message
+    del st.session_state.generated_message
+
+# press neither button to generate a new message pair
+st.button("Neither")
